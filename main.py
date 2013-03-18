0e722aa192639809653e884f7b5e8287d08ca5c4
admin <jonathan@ubuntu.(none)>
2013-03-17 20:49:01 -0400

    Learned what read().splitlines() does. This is a #learningnote #learning note: splitlines removes line breaks from the file or item you are reading.

--- main.py
+++ main.py
@@ -5,15 +5,13 @@
 #define a function that will return the year a film was made
 #split the right side of the line a the first "("
 def filmYear(film):
-	return film.rsplit ('(',1)[1] #My question: What does the [1] do?
+	return film.rsplit ('(',1)[1] 
 	
 #load the file into a list in Python memory
 #and then close the file because the content is now in memory
 with open ("filmlist.txt", "r") as file:
-	filmlist = file.read().splitlines() #My question: What does \
-	#splitlines() do?
+	filmlist = file.read().splitlines() 
 
 ##You skipped typing some stuff
 
 print filmlist ##My test.
-

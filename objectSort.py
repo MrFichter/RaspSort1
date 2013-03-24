fa3365f6b025e7479ecdaf0276ea3813b3c6efb2
FichterJ <jonathan@ubuntu.(none)>
2013-03-23 22:52:47 -0400

    It works! Tomorrow, I'll try using a regular expression to get rid of the "The" in the titles. I'll also change my for loop into list comprehension, which @fichtitious says is more memory efficient.

--- objectSort.py
+++ objectSort.py
@@ -9,8 +9,17 @@ class Movie (object): #Shows I'm making a basic Python data type.
 		self.name = name
 		self.year = year
 
+
+	def __repr__(self):
+		'''When I call print below, it will access repr.'''
+		return self.year + ' ' + self.name
+
+
+
+movies = []
+
 for line in open ("filmList.txt", "r"):
-	search = re.search('([A-z 0-9]*) \((.*)\)' , line )# When I have \
+	search = re.search('([A-z 0-9\-!\:]*) \((.*)\)' , line )# When I have \
 #parentheses not preceeded by an escape character, I'm telling re to \
 #be sensitive to the groups within the pattern. I can then use re.group.
 
@@ -21,6 +30,27 @@ for line in open ("filmList.txt", "r"):
 #upper or lower case, from A to Z, the space character, and digits.) \
 #The star means I could have any number of them.
 
-	print search.groups()
+#I also included characters like dash, colon, and exclamation point.
+
+	movie = Movie(*search.groups())
+# The above is the same as saying Movie (search.groups()[0], \
+#search.groups()[1]) , but it tells Python to automatically unpack the \
+#tuple.
+
+#calling Movie above runs the constructor function, but I have to save \
+#it to a variable.
+	movies.append(movie)
+
+movies.sort (key = lambda movie: movie.name) #sort by name, the secondary\
+#criterion.
+
+# I created a temporary function that takes the argument movie. 
+
 	
+movies.sort (key = lambda movie: movie.year) #sort by year, the \
+#primary criterion.
+
+
+for movie in movies: print movie 
+
 

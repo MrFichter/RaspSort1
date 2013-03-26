4934fc7150268927466362334360f77226a834ce
admin <jonathan@ubuntu.(none)>
2013-03-26 07:30:13 -0400

    Failed attempt at list comprehension. The previous commit worked fine without list comprehension. The next step will be to create a simplified example and ask for help on reddit.

--- objectSort.py
+++ objectSort.py
@@ -2,7 +2,7 @@
 
 import re
 
-class Movie (object): #Shows I'm making a basic Python data type.
+class Film (object): #Shows I'm making a basic Python data type.
 
 
 	def __init__(self, name, year):
@@ -15,42 +15,30 @@ class Movie (object): #Shows I'm making a basic Python data type.
 		return self.year + ' ' + self.name
 
 
-
-movies = []
+filmList = []
 
 for line in open ("filmList.txt", "r"):
-	search = re.search('([A-z 0-9\-!\:]*) \((.*)\)' , line )# When I have \
-#parentheses not preceeded by an escape character, I'm telling re to \
-#be sensitive to the groups within the pattern. I can then use re.group.
 
-#I originally used re.match, but match starts at the very beginning of \
-#the file, where there was some crud. I also originally had two .* \
-#expressions, but I turned the first one into square brackets followed \
-#by stars to make sure it only picks up clean characters. (Anything, \
-#upper or lower case, from A to Z, the space character, and digits.) \
-#The star means I could have any number of them.
+	filmList.append(line)
 
-#I also included characters like dash, colon, and exclamation point.
 
-	movie = Movie(*search.groups())
-# The above is the same as saying Movie (search.groups()[0], \
-#search.groups()[1]) , but it tells Python to automatically unpack the \
-#tuple.
 
-#calling Movie above runs the constructor function, but I have to save \
-#it to a variable.
-	movies.append(movie)
+search = re.search('([A-z 0-9\-!\:]*) \((.*)\)' , line )
 
-movies.sort (key = lambda movie: movie.name) #sort by name, the secondary\
+filmListObjects = [Film(*search.groups()) for re.search('([A-z 0-9\-!\:]*) \((.*)\)' , film ) for film in filmList
+
+
+
+filmListObjects.sort (key = lambda film: film.name) #sort by name, the secondary\
 #criterion.
 
 # I created a temporary function that takes the argument movie. 
 
 	
-movies.sort (key = lambda movie: movie.year) #sort by year, the \
+filmListObjects.sort (key = lambda film: film.year) #sort by year, the \
 #primary criterion.
 
 
-for movie in movies: print movie 
+for film in filmListObjects: print film
 
 
